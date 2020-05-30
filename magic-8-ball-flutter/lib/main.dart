61a84c2862434e6010d082ef8d71590e9fbae18b
roycechua23 <royce236@gmail.com>
2020-05-30 21:55:04 +0800

    Add magic 8 ball

---
+++ magic-8-ball-flutter/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage()
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask my anything'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Center(
+        child: Ball(),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: () {
+        setState(() {
+          ballNumber = Random().nextInt(5) + 1;
+        });
+      },
+      child: Image.asset('images/ball$ballNumber.png'),
+    );
+  }
+}
