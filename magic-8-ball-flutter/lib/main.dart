66acb06591dae6a5db090f8900f73518ba8d1d88
roycechua23 <royce236@gmail.com>
2020-05-30 21:56:23 +0800

    Modify Appbar title to Ask me instead of my

--- magic-8-ball-flutter/lib/main.dart
+++ magic-8-ball-flutter/lib/main.dart
@@ -13,7 +13,7 @@ class BallPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.blue,
       appBar: AppBar(
-        title: Text('Ask my anything'),
+        title: Text('Ask me anything'),
         backgroundColor: Colors.blue[900],
       ),
       body: Center(
